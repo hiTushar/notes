0d32d19d6527154469123f73cd9c757c2c93b334
Tushar Jain <tushar@antrepriz.com>
2023-10-16 16:56:46 +0530

    Add pagination

--- src/Components/NoteList/NoteList.js
+++ src/Components/NoteList/NoteList.js
@@ -1,11 +1,17 @@
-import { ListGroup, Row, Col, Modal, Button } from "react-bootstrap";
+import { ListGroup, Row, Col, Modal, Button, PageItem, Pagination } from "react-bootstrap";
 import moment from 'moment';
 import { useEffect, useState } from "react";
 import Controls from "./Controls";
+import PaginationComponent from "./Pagination";
+
+const notes_per_page = 5;
 
 export default function NoteList() {
-    const [notes, setNotes] = useState(null)
-    const [displayControls, setDisplayControls] = useState(true);
+    const [notes, setNotes] = useState(null);
+    const [notesDisplay, setNotesDisplay] = useState(null);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
+    const [displayControls, setDisplayControls] = useState(false);
     const [deleteModal, setDeleteModal] = useState(false);
 
     useEffect(() => {
@@ -13,8 +19,26 @@ export default function NoteList() {
         if (allNotes) {
             allNotes = JSON.parse(allNotes);
         }
-        setNotes(allNotes)
+        else {
+            allNotes = [];
+        }
+        setNotes(allNotes);
+        setNotesDisplay(allNotes.slice(currentPage, notes_per_page));
+    
+        let totalNoOfPages = 0;
+        if(allNotes.length % notes_per_page !== 0) {
+            totalNoOfPages = parseInt(allNotes.length / notes_per_page) + 1;
+        }
+        else {
+            totalNoOfPages = parseInt(allNotes.length / notes_per_page);
+        }
+        setTotalPage(totalNoOfPages);
     }, [])
+
+    useEffect(() => {
+        if(notes !== null)
+            setNotesDisplay(notes.slice(currentPage, notes_per_page));
+    }, [currentPage, notes])
 
     const deleteNote = (noteId) => {
         let newNotesList = notes.filter(note => note.id !== noteId);
@@ -25,34 +49,50 @@ export default function NoteList() {
 
     const getList = () => {
         return (
+            <ListGroup>
+                {notesDisplay.map((note) => (
+                    <div onMouseEnter={() => setDisplayControls(true)} onMouseLeave={() => setDisplayControls(false)}>
+                        <ListGroup.Item key={note.id}>
+                            <Row>
+                                <Col>
+                                    <b>{moment(note.timestamp).format('hh:mm A, Do MMM \'YY, dddd')}</b> - {note.title}
+                                </Col>
+                                <Col>
+                                    {
+                                        displayControls ? (
+                                            <div className='d-flex justify-content-end'>
+                                                <Controls openDelete={() => setDeleteModal(note.id)} />
+                                            </div>
+                                        ) : <></>
+                                    }
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    </div>
+                ))}
+            </ListGroup>
+        );
+    };
+
+    console.log(currentPage);
+    return (
+        <div>{notes && (
             <>
-                <ListGroup>
-                    {notes.map((note) => (
-                        <div onMouseEnter={() => setDisplayControls(true)} onMouseLeave={() => setDisplayControls(false)}>
-                            <ListGroup.Item key={note.id}>
-                                <Row>
-                                    <Col>
-                                        <b>{moment(note.timestamp).format('hh:mm A, Do MMM \'YY, dddd')}</b> - {note.title}
-                                    </Col>
-                                    <Col>
-                                        {
-                                            displayControls ? (
-                                                <div className='d-flex justify-content-end'>
-                                                    <Controls openDelete={() => setDeleteModal(note.id)} />
-                                                </div>
-                                            ) : <></>
-                                        }
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                        </div>
-                    ))}
-                </ListGroup>
+                <div>
+                    <div className='mb-2'>
+                        <PaginationComponent
+                            totalPage={totalPage} 
+                            currentPage={currentPage} 
+                            setCurrentPage={setCurrentPage}
+                            />
+                    </div>
+                    {getList()}
+                </div>
                 <Modal show={deleteModal} onHide={() => setDeleteModal(false)}>
-                    <Modal.Header>
-                        Modal
-                    </Modal.Header>
-                    <Modal.Footer>
+                    <Modal.Body>
+                        Delete this?
+                    </Modal.Body>
+                    <Modal.Footer style={{ borderTop: 'none' }}>
                         <Button variant='danger' onClick={() => deleteNote(deleteModal)}>
                             Delete
                         </Button>
@@ -62,10 +102,6 @@ export default function NoteList() {
                     </Modal.Footer>
                 </Modal>
             </>
-        );
-    };
-
-    return (
-        <div>{notes && getList()}</div>
+        )}</div>
     )
 }
---
+++ src/Components/NoteList/Pagination.js
@@ -0,0 +1,21 @@
+import { Pagination } from 'react-bootstrap';
+
+export default function PaginationComponent(props) {
+    const { totalPage, currentPage, setCurrentPage } = props;
+    
+    function getItems() {
+        let itemArray = [];
+        for(let i = 0; i < totalPage; i++) {
+            itemArray.push(<Pagination.Item key={i} active={i === currentPage} onClick={() => setCurrentPage(i)}>{i + 1}</Pagination.Item>);
+        }
+        return itemArray;
+    }
+
+    return (
+        <Pagination>
+            {
+                getItems()
+            }
+        </Pagination>
+    )
+}
